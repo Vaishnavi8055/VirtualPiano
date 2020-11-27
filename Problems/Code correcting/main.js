d6820e564a3a9c0cbd4257a03f315a13ebf96da9
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-11-27 18:47:09 +1400

    value of age is changed to 18

---
+++ Problems/Code correcting/main.js
@@ -0,0 +1,17 @@
+let age = 18;
+
+if (age < 6) {
+    console.log("Anna is a little child");
+}
+else if (age >= 6 && age < 18) {
+    console.log("Anna goes to school");
+}
+else if (age == 18) {
+    console.log("Anna is going to study at University");
+}
+else if (age > 18 && age < 22) {
+    console.log("Anna studies at University");
+}
+else {
+    console.log("Anna works");
+}
